013a5b0c3b9af0e05deb92adda87b447abe2428c
Sector <35609551+XSectoRX@users.noreply.github.com>
2024-07-13 21:59:06 +0300

    Build PROM
    PROM

--- src/pages/user-profile/index.tsx
+++ src/pages/user-profile/index.tsx
@@ -1,64 +1,132 @@
-import { Button, Textarea } from "@nextui-org/react"
-import { IoMdCreate } from "react-icons/io"
-import { useForm, Controller } from "react-hook-form"
-import { ErrorMessage } from "../error-message"
-import { useCreateCommentMutation } from "../../app/services/commentsApi"
+import { useEffect } from "react"
 import { useParams } from "react-router-dom"
-import { useLazyGetPostByIdQuery } from "../../app/services/postsApi"
+import {
+    useGetUserByIdQuery,
+    useLazyCurrentQuery,
+    useLazyGetUserByIdQuery,
+} from "../../app/services/userApi"
+import { useDispatch, useSelector } from "react-redux"
+import { resetUser, selectCurrent } from "../../features/user/userSlice"
+import { Button, Card, Image } from "@nextui-org/react"
+import { MdOutlinePersonAddAlt1 } from "react-icons/md"
+import { MdOutlinePersonAddDisabled } from "react-icons/md"
+import { useDisclosure } from "@nextui-org/react"
+import {
+    useFollowUserMutation,
+    useUnfollowUserMutation,
+} from "../../app/services/followApi"
+import { GoBack } from "../../components/go-back"
+import { BASE_URL } from "../../constants"
+import { CiEdit } from "react-icons/ci"
+import { EditProfile } from "../../components/edit-profile"
+import { formatToClientDate } from "../../utils/format-to-client-date"
+import { ProfileInfo } from "../../components/profile-info"
+import { CountInfo } from "../../components/count-info"
 
-export const CreateComment = () => {
+export const UserProfile = () => {
     const { id } = useParams<{ id: string }>()
-    const [createComment] = useCreateCommentMutation()
-    const [getPostById] = useLazyGetPostByIdQuery()
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const currentUser = useSelector(selectCurrent)
+    const { data } = useGetUserByIdQuery(id ?? "")
+    const [followUser] = useFollowUserMutation()
+    const [unfolowUser] = useUnfollowUserMutation()
+    const [triggerGetUserByIdQuery] = useLazyGetUserByIdQuery()
+    const [triggerCurrentQuery] = useLazyCurrentQuery()
 
-    const {
-        handleSubmit,
-        control,
-        formState: { errors },
-        setValue,
-    } = useForm()
+    const dispatch = useDispatch()
 
-    const onSubmit = handleSubmit(async (data) => {
+    useEffect(
+        () => () => {
+            dispatch(resetUser())
+        },
+        [],
+    )
+
+    const handleFollow = async () => {
         try {
             if (id) {
-                await createComment({ content: data.comment, postId: id }).unwrap()
-                await getPostById(id).unwrap()
-                setValue("comment", "")
+                data?.isFollowing
+                    ? await unfolowUser(id).unwrap()
+                    : await followUser({ followingId: id }).unwrap()
+
+                await triggerGetUserByIdQuery(id)
+
+                await triggerCurrentQuery()
             }
         } catch (error) {
-            console.log("err", error)
+            console.log(error)
         }
-    })
+    }
 
-    const error = errors?.comment?.message as string
+    const handleClose = async () => {
+        try {
+            if (id) {
+                await triggerGetUserByIdQuery(id)
+                await triggerCurrentQuery()
+                onClose()
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    if (!data) {
+        return null
+    }
 
     return (
-        <form className="flex-grow" onSubmit={onSubmit}>
-            <Controller
-                name="comment"
-                control={control}
-                defaultValue=""
-                rules={{
-                    required: "Поле обязательно",
-                }}
-                render={({ field }) => (
-                    <Textarea
-                        {...field}
-                        labelPlacement="outside"
-                        placeholder="Напишите свой ответ"
-                        className="mb-5"
+        <>
+            <GoBack />
+            <div className="flex items-stretch gap-4">
+                <Card className="flex flex-col items-center text-center space-y-4 p-5 flex-2">
+                    <Image
+                        src={`${BASE_URL}${data.avatarUrl}`}
+                        alt={data.name}
+                        width={200}
+                        height={200}
+                        className="border-4 border-white"
                     />
-                )}
-            />
-            {errors && <ErrorMessage error={error} />}
-            <Button
-                color="primary"
-                className="flex-end"
-                endContent={<IoMdCreate />}
-                type="submit"
-            >
-                Ответить
-            </Button>
-        </form>
+                    <div className="flex flex-col text-2xl font-bold gap-4 items-center">
+                        {data.name}
+                        {currentUser?.id !== id ? (
+                            <Button
+                                color={data?.isFollowing ? "default" : "primary"}
+                                variant="flat"
+                                className="gap-2"
+                                onClick={handleFollow}
+                                endContent={
+                                    data?.isFollowing ? (
+                                        <MdOutlinePersonAddDisabled />
+                                    ) : (
+                                        <MdOutlinePersonAddAlt1 />
+                                    )
+                                }
+                            >
+                                {data?.isFollowing ? 'Отписаться' : 'Подписаться'}
+                            </Button>
+                        ) : (
+                            <Button
+                                endContent={<CiEdit />}
+                                onClick={() => onOpen()}
+                            >
+                                Редактировать
+                            </Button>
+                        )}
+                    </div>
+                </Card>
+                <Card className="flex flex-col space-y-4 p-5 flex-1">
+                    <ProfileInfo title="Почта:" info={data.email} />
+                    <ProfileInfo title="Местоположение:" info={data.location} />
+                    <ProfileInfo title="Дата рождения:" info={formatToClientDate(data.dateOfBirth)} />
+                    <ProfileInfo title="Обо мне:" info={data.bio} />
+
+                    <div className="flex gap-2">
+                        <CountInfo count={data.followers.length} title="Подписчики"/>
+                        <CountInfo count={data.following.length} title="Подписки"/>
+                    </div>
+                </Card>
+            </div>
+            <EditProfile isOpen={isOpen} onClose={handleClose} user={data} />
+        </>
     )
 }
